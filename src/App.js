0f170cf564583f39e7aa1b590b89e13741235304
unknown <mohdhuzaifa8126195456@gmail.com>
2024-05-07 12:46:19 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+import { useState } from 'react'
+import ContactsForm from './components/ContactsForm'
+import ContactView from './components/ContactView';
+import Home from './Home'
 import './App.css';
+import MapAndCharts from './MapAndCharts';
+import { Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar';
+import NavigationButton from './components/NavigationButton'
+
+
 
 function App() {
+  const [open, setOpen] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <CustomeSidebar /> */}
+      <div className='flex'>
+        <Sidebar open={open} setOpen={setOpen} />
+        <div className='fixed top-3 left-2 h-8 w-8 rounded-full'>
+          <NavigationButton open={open} setOpen={setOpen} />
+        </div>
+        <div className='w-full'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/maps' element={<MapAndCharts />} />
+            <Route exact path='/user/:id' element={<ContactView />} />
+            <Route exact path='/create-contact' element={<ContactsForm />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/CustomeSidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomeSidebar() {
+    return (
+        <div>CustomeSidebar</div>
+    )
+}
+
+export default CustomeSidebar
